16143a32fc45758747b1c3663fc1ee4cb9d19b27
Mropat <mariaropat@gmail.com>
2021-08-11 11:51:02 +0200

    Add top level payload models

--- cgdeployment/endpoints/webhook.py
+++ cgdeployment/endpoints/webhook.py
@@ -1,5 +1,12 @@
 from typing import Optional
 
+from cgdeployment.models import (
+    PushPayload,
+    IssuesPayload,
+    IssueCommentsPayload,
+    ReleasePayload,
+    PullRequestPayload,
+)
 from fastapi import FastAPI, File, Form, HTTPException, Response, UploadFile, Request
 from pydantic import NameEmail, BaseSettings, BaseModel
 from starlette.responses import PlainTextResponse
@@ -8,9 +15,6 @@ app = FastAPI()
 
 
 class Payload(BaseModel):
-    action: Optional[str]
-    issue: Optional[dict]
-
     class Config:
         extra = "allow"
 
@@ -20,7 +24,36 @@ def inform_error(request, exc):
     return PlainTextResponse(str(exc.detail), status_code=exc.status_code)
 
 
-@app.post("/payload")
-async def payload(payload: Payload):
+@app.post("/push")
+async def payload(payload: PushPayload):
+    print("push post")
+    print(payload)
+    return Response(status_code=200)
+
+
+@app.post("/issue_comment")
+async def payload(payload: IssueCommentsPayload):
+    print("issue comment post")
+    print(payload)
+    return Response(status_code=200)
+
+
+@app.post("/release")
+async def payload(payload: ReleasePayload):
+    print("release post")
+    print(payload)
+    return Response(status_code=200)
+
+
+@app.post("/pull_request")
+async def payload(payload: PullRequestPayload):
+    print("pull request post")
+    print(payload)
+    return Response(status_code=200)
+
+
+@app.post("/issues")
+async def payload(payload: IssuesPayload):
+    print("issue post")
     print(payload)
     return Response(status_code=200)
---
+++ cgdeployment/models.py
@@ -0,0 +1,47 @@
+from typing import Optional
+
+from pydantic import BaseModel
+
+
+class Payload(BaseModel):
+    class Config:
+        extra = "allow"
+
+
+class ReleasePayload(Payload):
+    action: Optional[str]
+    changes: Optional[Payload]
+    issue: Optional[Payload]
+    release: Optional[dict]
+
+
+class IssueCommentsPayload(Payload):
+    action: Optional[str]
+    changes: Optional[Payload]
+    issue: Optional[Payload]
+    comment: Optional[Payload]
+
+
+class IssuesPayload(Payload):
+    action: Optional[str]
+    changes: Optional[Payload]
+    issue: Optional[Payload]
+    assignee: Optional[Payload]
+    label: Optional[Payload]
+
+
+class PullRequestPayload(Payload):
+    action: Optional[str]
+    number: Optional[int]
+    changes: Optional[Payload]
+    pull_request: Optional[Payload]
+
+
+class PushPayload(Payload):
+    push_id: Optional[int]
+    size: Optional[int]
+    distinct_size: Optional[int]
+    ref: Optional[str]
+    head: Optional[str]
+    before: Optional[str]
+    commits: Optional[Payload]
