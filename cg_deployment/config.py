eadc3028ac0b2eb1ddab22a209609469c4b5cf21
Vincent Janvid <vincent.janvid@scilifelab.se>
2025-05-05 16:47:12 +0200

    Set logger level to Info

--- cg_deployment/config.py
+++ cg_deployment/config.py
@@ -1,7 +1,9 @@
+import logging
 from typing import Optional
 
 from pydantic import BaseSettings, validator
 
+logging.basicConfig(level=logging.INFO)
 
 class EnvConfig(BaseSettings):
     authorization_token: Optional[str]
